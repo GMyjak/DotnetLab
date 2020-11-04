7f703d4fca2e2df87e35e005668799e4b77e9663
g.myjak <g.myjak@neurosys.com>
2020-11-04 13:49:54 +0100

    Add lab4, lab5

---
+++ Lab4/Program.cs
@@ -0,0 +1,195 @@
+﻿//#define VERBOSE
+
+using System;
+using System.Text;
+
+namespace Lab4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Change values in if statements to true to perform tests
+
+            if (true)
+            {
+                var res = GetFromConsoleXY("First num", "Second num");
+                Console.WriteLine($"N1: {res.Item1}, N2: {res.Item2}");
+
+                GetFromConsoleXY("Num 1", "Num 2", out int x, out int y);
+                Console.WriteLine($"N1: {x}, N2: {y}");
+            }
+
+            if (false)
+            {
+                // Standard call
+                DrawCard("Dzień dobry", "Wizytówka", 'O', 1, 25);
+                Console.WriteLine();
+
+                // Only obligatory params
+                DrawCard("Dzień dobry");
+                Console.WriteLine();
+
+                // First three params
+                DrawCard("Dzień dobry", "Wizytóweczka", 'H');
+                Console.WriteLine();
+
+                // Last two params swapped place
+                DrawCard("Dzień dobry", "Wizytówka", frameWidth: 1, frame: 'O');
+                Console.WriteLine();
+
+                // All non obligatory params in random order
+                DrawCard("Dzień dobry", minCardWidth: 30, frame: 'X', secondLine: "String", frameWidth: 1);
+                Console.WriteLine();
+            }
+
+            if (false)
+            {
+                CountMyTypes(out int ints, out int reals, out int strings, out int others, 2, 4, 25, 2.0, -2.0, "XD",
+                    "KOT", "BATERIA");
+                Console.WriteLine("Even integers: " + ints);
+                Console.WriteLine("Positive real numbers: " + reals);
+                Console.WriteLine("5 or more length strings: " + strings);
+                Console.WriteLine("Other values: " + others);
+            }
+        }
+
+        // TASK 1a
+        static (int, int) GetFromConsoleXY(string com1, string com2)
+        {
+            Console.WriteLine(com1);
+            int x = int.Parse(Console.ReadLine());
+            Console.WriteLine(com2);
+            int y = int.Parse(Console.ReadLine());
+            #if VERBOSE
+            Console.WriteLine("Returning values in tuple");
+            #endif
+            return (x, y);
+        }
+
+        // TASK 1b
+        static void GetFromConsoleXY(string com1, string com2, out int x, out int y)
+        {
+            Console.WriteLine(com1);
+            x = int.Parse(Console.ReadLine());
+            #if VERBOSE
+            Console.WriteLine("Saved first value in out x");
+            #endif
+            Console.WriteLine(com2);
+            y = int.Parse(Console.ReadLine());
+            #if VERBOSE
+            Console.WriteLine("Saved second value in out y");
+            #endif
+        }
+
+        // TASK 2
+        static void DrawCard(string firstLine, string secondLine = "Nowak", char frame = 'X', int frameWidth = 2, int minCardWidth = 20)
+        {
+            // Calculate cardWidth
+            int cardWidth = Math.Max(minCardWidth, frameWidth * 2 + Math.Max(firstLine.Length, secondLine.Length) + 2);
+
+            // Generate upper bounds
+            var bounds = DrawBounds(frameWidth, cardWidth, frame).ToString();
+
+            // Generate first line
+            var line1 = DrawCardLine(firstLine, cardWidth, frameWidth, frame).ToString();
+
+            // Generate second line
+            var line2 = DrawCardLine(secondLine, cardWidth, frameWidth, frame).ToString();
+
+            #if VERBOSE
+            Console.WriteLine("Drawing card:");
+            #endif
+            Console.Write(bounds);
+            Console.Write(line1);
+            Console.Write(line2);
+            Console.Write(bounds);
+        }
+
+        // HELPER FOR TASK 2
+        static StringBuilder DrawBounds(int width, int cardWidth, char frame)
+        {
+            #if VERBOSE
+            Console.WriteLine("Generating card bounds");
+            #endif
+            StringBuilder result = new StringBuilder();
+            for (int i = 0; i < width; i++)
+            {
+                for (int j = 0; j < cardWidth; j++)
+                {
+                    result.Append(frame);
+                }
+                result.Append("\n");
+            }
+            return result;
+        }
+
+        // HELPER FOR TASK 2
+        static StringBuilder DrawCardLine(string message, int cardWidth, int frameWidth, char frame)
+        {
+            #if VERBOSE
+            Console.WriteLine("Generating card line");
+            #endif
+            int firstStrSpacing = cardWidth - frameWidth - frameWidth - message.Length;
+            StringBuilder result = new StringBuilder();
+            for (int i = 0; i < frameWidth; i++)
+            {
+                result.Append(frame);
+            }
+            for (int i = 0; i < firstStrSpacing / 2; i++)
+            {
+                result.Append(' ');
+            }
+            result.Append(message);
+            firstStrSpacing = firstStrSpacing % 2 == 0 ? firstStrSpacing / 2 : firstStrSpacing / 2 + 1;
+            for (int i = 0; i < firstStrSpacing; i++)
+            {
+                result.Append(' ');
+            }
+            for (int i = 0; i < frameWidth; i++)
+            {
+                result.Append(frame);
+            }
+            result.Append("\n");
+            return result;
+        }
+
+        //TASK 3
+        static void CountMyTypes(out int ints, out int reals, out int strings, out int others, params object[] arguments)
+        {
+            ints = 0;
+            reals = 0;
+            strings = 0;
+            others = 0;
+
+            foreach (object obj in arguments)
+            {
+                switch (obj)
+                {
+                    case int integer when (integer % 2 == 0):
+                        #if VERBOSE
+                        Console.WriteLine("CountMyTypes: integer detected");
+                        #endif
+                        ints++;
+                        break;
+                    case double real when (real > 0):
+                        #if VERBOSE
+                        Console.WriteLine("CountMyTypes: double detected");
+                        #endif
+                        reals++;
+                        break;
+                    case string str when (str.Length >= 5):
+                        #if VERBOSE
+                        Console.WriteLine("CountMyTypes: string detected");
+                        #endif
+                        strings++;
+                        break;
+                    default:
+                        others++;
+                        break;
+                }
+            }
+        }
+
+    }
+}
---
+++ Lab5/Program.cs
@@ -0,0 +1,177 @@
+﻿using System;
+
+namespace Lab5
+{
+    class Program
+    {
+        static void Main()
+        {
+            // TASK 1 TESTS
+            Console.WriteLine($"Simplify counter: {MixedNumber.SimplifierCounter}");
+            MixedNumber a = new MixedNumber();
+            Console.WriteLine($"A: {a}");
+            a.Whole = 12;
+            // Here number will be simplified from 12+0/6 to 12+0/1 ...
+            a.Denominator = 6;
+            // ... so this line will add 10 to whole
+            a.Numerator = 10;
+            Console.WriteLine($"A after setting values: {a}");
+
+            MixedNumber b = new MixedNumber(12, 5);
+            MixedNumber c = new MixedNumber(7, 3, 10);
+            Console.WriteLine($"B: {b}");
+            Console.WriteLine($"C: {c}");
+            Console.WriteLine($"B fraction as double: {b.FractionAsDouble}");
+            Console.WriteLine($"C fraction as double: {c.FractionAsDouble}");
+            Console.WriteLine($"B as double: {b.NumberAsDouble}");
+            Console.WriteLine($"C as double: {c.NumberAsDouble}");
+            MixedNumber d = b + c;
+            Console.WriteLine($"D = B + C: {d}");
+            Console.WriteLine($"B + D: {b + d}");
+            Console.WriteLine($"Simplify counter after operations: {MixedNumber.SimplifierCounter}");
+
+            // TASK 2 TEST
+            Console.WriteLine("ASDFQWER!@#$%^&*".ModifyString());
+        }
+    }
+
+    // TASK 1
+    class MixedNumber
+    {
+        // DEFINE PROPERTIES
+        public int Whole { get; set; }
+        
+        private int numerator;
+        public int Numerator
+        {
+            get { return numerator; }
+            set { numerator = Math.Abs(value); SimplifyFraction(); }
+        }
+
+        private int denominator;
+        public int Denominator
+        {
+            get { return denominator; }
+            set 
+            {
+                if (value == 0)
+                    throw new DivideByZeroException();
+                denominator = Math.Abs(value); 
+                SimplifyFraction();
+            } 
+        }
+
+        public double FractionAsDouble { get { return (double)numerator / denominator; } }
+        public double NumberAsDouble { get { return FractionAsDouble + Whole; } }
+        public static int SimplifierCounter { get; private set; }
+
+        // CONSTRUCTORS
+        public MixedNumber(int whole, int numerator, int denominator)
+        {
+            Whole = whole;
+            this.numerator = numerator;
+            this.denominator = denominator;
+            SimplifyFraction();
+        }
+
+        public MixedNumber(int numerator, int denominator) 
+            : this(0, numerator, denominator) {}
+
+        public MixedNumber() : this(0, 1) {}
+
+        // OPERATORS
+        public static MixedNumber operator +(MixedNumber a, MixedNumber b)
+        {
+            int lcm = LeastCommonMultiple(a.Denominator, b.Denominator);
+            int num1 = a.numerator * (lcm / a.Denominator);
+            int num2 = b.numerator * (lcm / b.Denominator);
+            MixedNumber res = new MixedNumber(a.Whole + b.Whole, num1 + num2, lcm);
+            res.SimplifyFraction();
+            return res;
+        }
+
+        // HELPER METHODS
+        private static int GreatestCommonDivisor(int a, int b)
+        {
+            while (a != 0 && b != 0)
+            {
+                if (a > b)
+                    a %= b;
+                else
+                    b %= a;
+            }
+
+            return a | b;
+        }
+
+        private static int LeastCommonMultiple(int a, int b)
+        {
+            int baseA = a, baseB = b;
+            while (a!=b)
+            {
+                if (a > b)
+                {
+                    b += baseB;
+                }
+                else
+                {
+                    a += baseA;
+                }
+            }
+            return a;
+        }
+
+        private void SimplifyFraction()
+        {
+            int gcd = GreatestCommonDivisor(numerator, denominator);
+            if (gcd > 1 || numerator >= denominator)
+                SimplifierCounter++;
+            Whole += numerator / denominator;
+            numerator %= denominator;
+            numerator /= gcd;
+            denominator /= gcd;
+        }
+
+        public override string ToString()
+        {
+            return $"{Whole}+{numerator}/{denominator}";
+        }
+    }
+
+    static class StringExtensions
+    {
+        // TASK 2
+        public static string ModifyString(this string source)
+        {
+            char[] result = source.ToCharArray();
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    if (char.IsLower(result[i]))
+                    {
+                        result[i] = char.ToUpper(result[i]);
+                    }
+                    else if (!char.IsUpper(result[i]))
+                    {
+                        result[i] = '.';
+                    }
+                }
+                else
+                {
+                    if (char.IsUpper(result[i]))
+                    {
+                        result[i] = char.ToLower(result[i]);
+                    }
+                    else if (!char.IsLower(result[i]))
+                    {
+                        result[i] = '.';
+                    }
+                }
+            }
+
+            return new string(result);
+        }
+    }
+}
